104aaaa642def9b86238b85e94a77ae32afdf77b
vy nguyen <vyn@metropolia.fi>
2019-11-15 12:59:37 +0200

    serving static files

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict'
 const express = require('express')
-const app = express()
+const app = express(); 
+app.use(express.static('public'));
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+	console.log('request', req);
+	res.send('demo');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
