da7d32a7ca9ad64b895f7c087e87c4b62b4e2dfe
vy nguyen <vyn@metropolia.fi>
2019-11-17 14:57:57 +0200

    connect to database and run queries

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
 'use strict';
 
-require('dotenv').config();
-
 const express = require('express');
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
+const connection = require('./model/db.js');
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/animal',(req,res) =>{
-    connection.query(
-        'SELECT * FROM animal',
-        (err,results,fields) => {
-            console.log(results);
-            console.log(fields);
-            res.json(results);
-        }
-    )
-})
-
-
-app.get('/',(request,response)=>{
-    response.send('Hello from my Node server');
+app.get('/animal', async (req, res) => {
+    try {
+        const [results, fields] = await connection.query('SELECT animal.* FROM animal');
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+        res.json(results);
+    } catch (e) {
+        console.log(e);
+        res.send('db error :(');
+    }
 });
 
-app.get('/demo', (request,response)=>{
-    console.log('request',request);
-    response.send('demo')
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from my Node Server');
 });
 
-app.listen(3000, ()=>{
-    console.log('server app start?')
+app.get('/demo', (req, res) => {
+    console.log('request', req);
+    res.send('demo');
+});
+
+app.listen(3000, () => {
+    console.log('Server app start?');
 });
 
