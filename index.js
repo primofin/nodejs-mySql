f9cfe5d42839d052c5f38eacb74c4da00ee8d903
vy nguyen <vyn@metropolia.fi>
2019-11-21 15:12:22 +0200

    https

--- index.js
+++ index.js
@@ -1,57 +1,62 @@
+
 'use strict';
 
 const express = require('express');
-const connection = require('./model/db.js');
-const bodyParser = require('body-parser');
 const animal = require('./model/animal');
 
 const app = express();
 
+if(process.env.SERVER === 'dev_localhost') {
+  require('./secure/localhost')(app);
+} else {
+  require('./secure/server')(app);
+  app.listen(3000, () => {
+    console.log('server app start?');
+  });
+}
+
+const bodyParser = require('body-parser');
+
 app.use(express.static('public'));
 
 app.get('/animals', async (req, res) => {
-    try {
-        res.json(await animal.getAll());
-    } catch (e) {
-        console.log(e);
-        res.send('db error :(');
-    }
+  try {
+    res.json(await animal.getAll());
+  } catch (e) {
+    console.log(e);
+    res.send('db error :(');
+  }
 });
 
-
-app.get('/animal', async (req,res) => {
-    console.log(req.query);
-    //res.send(`query param? ${req.query}`);
-    try {
-        res.json(await animal.search(req.query.name));
-        
-    } catch(e) {
-        res.send(`db error ${e}`);
-    }
-
+app.get('/animal', async (req, res) => {
+  console.log(req.query);
+  try {
+    res.json(await animal.search(req.query.name));
+  } catch(e) {
+    res.send(`db error`);
+  }
 });
 
-app.post('/animal', bodyParser.urlencoded({extended:true}), async (req, res) => {
-    console.log(req.body);
-    try {
-        res.json(await animal.insert(req.body.name));
-    } catch (e){
-        console.log(e);
-        res.send('db error');
-    }
-    
+app.post('/animal', bodyParser.urlencoded({extended: true}), async (req, res) => {
+  console.log(req.body);
+  try {
+    res.json(await animal.insert(req.body.name));
+  } catch (e) {
+    console.log(e);
+    res.send('db error');
+  }
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello from my Node Server');
+  if(req.secure) {
+    res.send('Hello secure');
+  } else {
+    res.send('Hello form my Node server unsecure');
+  }
 });
 
 app.get('/demo', (req, res) => {
-    console.log('request', req);
-    res.send('demo');
-});
-
-app.listen(3000, () => {
-    console.log('Server app start?');
+  console.log('request', req);
+  res.send('demo');
 });
 
